cc11becf34badd3f4fef7e66d13f49e9aced1f6f
Jake Miller <jake0miller@gmail.com>
2019-06-24 17:39:11 -0600

    Initial commit

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "merchants index page", type: :feature do
+  it "user can see all merchants" do
+    merchant_1 = Merchant.create(name: 'Bob', address: '400 W 10th', city: 'Denver', state: 'CO', zip: 80204)
+    merchant_2 = Merchant.create(name: 'Roy', address: '601 W 11th', city: 'Chicago', state: 'IL', zip: 50291)
+
+    visit "/merchants"
+
+    expect(page).to have_content(merchant_1.title)
+    expect(page).to have_content("Play Count: #{merchant_1.play_count}")
+    expect(page).to have_content(merchant_2.title)
+    expect(page).to have_content("Play Count: #{merchant_2.play_count}")
+  end
+end
