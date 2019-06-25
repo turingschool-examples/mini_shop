310a05ed719ce203d97daadcfd180c1c97cedb76
Jori Peterson <joripeterson@gmail.com>
2019-06-24 20:34:51 -0600

    Update files for first and second user stories

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,4 +6,37 @@ class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  # def new
+  # end
+  #
+  # def create
+  #   merchant = Merchant.new({
+  #     title: params[:task][:title],
+  #     description: params[:task][:description]
+  #     })
+  #
+  #   merchant.save
+  #
+  #   redirect_to '/merchants'
+  # end
+  #
+  # def edit
+  #   @merchant = Merchant.find(params[:id])
+  # end
+  #
+  # def update
+  #   merchant = Merchant.find(params[:id])
+  #   merchant.update({
+  #     title: params[:merchant][:title],
+  #     description: params[:merchant][:description]
+  #     })
+  #   merchant.save
+  #   redirect_to "/merchants/#{merchant.id}"
+  # end
+  #
+  # def destroy
+  #   Merchant.destroy(params[:id])
+  #   redirect_to '/merchants'
+  # end  
 end
