4274861a5c5eedaf2b6b56c914e50a572b3a220b
Sarah Tatro <statrox4@gmail.com>
2019-06-24 18:46:11 -0600

    Create Merchant index feature test

---
+++ spec/features/merchant/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Index' do
+  describe "As a Visitor" do
+    it "I see a list of all Merchants" do
+      merchant_1 = Merchant.create(name: 'bobs burgers')
+      merchant_2 = Merchant.create(name: 'sallys fries')
+
+      visit '/merchants'
+
+      expect(page).to have_content(merchant_1.name)
+      expect(page).to have_content(merchant_2.name)
+    end
+  end
+end
