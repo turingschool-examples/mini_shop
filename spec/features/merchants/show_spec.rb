bdd412d867a46ee7fb53c4ab37948b863414a4b4
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-25 21:23:32 -0600

    Add test to check for merchant's name, address, city, and zip

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe "merchants show page", type:
 :feature do
   it "user can see merchant corresponding to id including the merchant's name, address, city, state, and zip" do
     merchant_1 = Merchant.create(name: "Thai Tanic", address: "12600 Feet Under the Sea", city: "North Atlantic Ocean", State: "SOS", Zip: 1912)
-    # merchant_2 = Merchant.create(name: "Lord Of The Rinse", address: "Mount Doom", city:"Middle Earth", zip: 2001)
-    # merchant_3 = Merchant.create(name: "Pita Pan", address:"Mythical Island of Neverland", city:"Neverland" , zip:1904 )
+    
 
     visit "/merchants/#{merchant_1.id}"
 
