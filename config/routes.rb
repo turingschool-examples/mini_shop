8bcca0db8d430e132c89dc75b3d1f9b22b3b7e19
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-25 21:31:21 -0600

    Add route for getting new merchant page and posting new merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/merchants', to: 'merchants#index'
   get '/merchants/:id', to: 'merchants#show'
+  get '/merchants/new', to: 'merchants#new'
+  post '/merchants', to: 'merchants#create'
 end
