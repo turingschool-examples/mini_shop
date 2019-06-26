494070b3e18911cb87e76f27c3b24fd6a74dc631
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-06-26 10:50:10 -0600

    Adds Merchant name is link to show:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+merch_1 = Merchant.create(name: "Billy", address: "123 Ya moms house Circle", city: "Chicago",
+  state: "Illinois", zip: 78047)
+merch_2 = Merchant.create(name: "Mr. Guy", address: "1 big ole' Drive", city: "Honolulu",
+  state: "Hawaii", zip: 11111)
+merch_3 = Merchant.create(name: "Penny Pinchin' Paul", address: "345 10th St", city: "Denver",
+    state: "Colorado", zip: 80204)
+merch_4 = Merchant.create(name: "Money Mike", address: "$$ Money Drive", city: "Cashville",
+    state: "New York", zip: 90876)
