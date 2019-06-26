2455468cece0f701dea7739ee4d746997b81a7d8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-26 14:13:11 -0600

    Move position of route for new merchant page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/merchants', to: 'merchants#index'
-  get '/merchants/:id', to: 'merchants#show'
   get '/merchants/new', to: 'merchants#new'
+  get '/merchants/:id', to: 'merchants#show'
   post '/merchants', to: 'merchants#create'
 end
