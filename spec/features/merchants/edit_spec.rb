b449890cfdfe6dd6ac5bff76ec42cc7f2420082c
Stella Mainar <mainars09@alumni.hanover.edu>
2019-06-27 13:26:26 -0600

    Add feature test for Merchant Update

---
+++ spec/features/merchants/edit_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Update", type: :feature do
+  describe "As a visitor" do
+    before(:each) do
+      @merchant_1 = Merchant.create!(name: "Merchant 1", address: "Address 1", city: "City 1", state: "State 1", zip: "Zip 1")
+    end
+
+    describe "When I click the link 'Edit Merchant Info' on the show page" do
+      it "I  see a form to edit the merchant's data" do
+        visit "/merchants/#{@merchant_1.id}"
+
+        expect(page).to have_link("Edit Info")
+
+        click_link "Edit Info"
+        expect(current_path).to eq("/merchants/#{@merchant_1.id}/edit")
+
+        new_address = "456 Winter Drive"
+        new_city = "Boulder"
+        new_zip = "12345"
+
+        fill_in "Address", with: new_address
+        fill_in "City", with: new_city
+        fill_in "Zip", with: new_zip
+
+        click_button "Submit Update"
+
+        expect(current_path).to eq("/merchants/#{@merchant_1.id}")
+        expect(page).to have_content(new_address)
+        expect(page).to have_content(new_city)
+        expect(page).to have_content(new_zip)
+      end
+    end
+  end
+end
