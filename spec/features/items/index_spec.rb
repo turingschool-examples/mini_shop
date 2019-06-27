7035e3c8f136f0accddf22da040eb185453083a0
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-27 12:15:01 -0600

    Fix merchant name field

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe "Items Index" do
   describe "As a visitor" do
     it "I see each item and its info in the system" do
-      item_1 = Item.create(name: "Galaxy Hops", description: "Huge hop oil content with pungent citrus and passion fruit flavors.", price: 36.99, image: "/app/assets/images/galaxy_hops.png", active: true, inventory: 28, merchant: "What Ales You")
+      merchant_1 = Merchant.create(name: "What Ales You")
+      item_1 = Item.create(name: "Galaxy Hops", description: "Huge hop oil content with pungent citrus and passion fruit flavors.", price: 36.99, image: "/app/assets/images/galaxy_hops.png", active: true, inventory: 28, merchant_name: merchant_1.name, merchant_id: merchant_1.id)
 
       visit '/items'
 
