1b678f96a28c571dd74607e5576d66fe2fbc2774
Sejin Kim <froydroyce@gmail.com>
2019-06-27 14:40:44 -0600

    Add development instantiations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Merchant.destroy_all
+merchant_1 = Merchant.create!(
+  name: "Knees Weak",
+  address: "1 Oppurtunity Way",
+  city: "Denver",
+  state: "Colorado",
+  zip: 80229
+)
+
+merchant_2 = Merchant.create(
+  name: "Mom's Spaghetti",
+  address: "1 Moment Way",
+  city: "Las Vegas",
+  state: "Nevada",
+  zip: 80003
+)
+
+merchant_3 = Merchant.create(
+  name: "So Loud",
+  address: "8 Mile rd",
+  city: "Los Angeles",
+  state: "California",
+  zip: 99482
+)
+
+merchant_4 = Merchant.create(
+  name: "Calm and Ready",
+  address: "1 Shot Way",
+  city: "St. Paul",
+  state: "Minnesota",
+  zip: 82783
+)
