3783bf56d2b4c7e59fe1cb9c523e10d67aeb4c17
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-27 13:36:46 -0600

    Fix image name

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Items Index" do
   describe "As a visitor" do
     it "I see each item and its info in the system" do
       merchant_1 = Merchant.create(name: "What Ales You")
-      item_1 = Item.create(name: "Galaxy Hops", description: "Huge hop oil content with pungent citrus and passion fruit flavors.", price: 36.99, image: "/app/assets/images/galaxy_hops.png", active: true, inventory: 28, merchant_name: merchant_1.name, merchant_id: merchant_1.id)
+      item_1 = Item.create(name: "Galaxy Hops", description: "Huge hop oil content with pungent citrus and passion fruit flavors.", price: 36.99, image: "galaxy_hops.png", active: true, inventory: 28, merchant_name: merchant_1.name, merchant_id: merchant_1.id)
 
       visit '/items'
 
@@ -14,7 +14,7 @@ RSpec.describe "Items Index" do
       expect(page).to have_content(item_1.image)
       expect(page).to have_content(item_1.active)
       expect(page).to have_content(item_1.inventory)
-      expect(page).to have_content(item_1.merchant)
+      expect(page).to have_content(item_1.merchant_name)
     end
 
   end
