e2e52d1e9ee598ca4bf3b41f2e6a4deb5f15b86d
Sejin Kim <froydroyce@gmail.com>
2019-06-29 11:33:28 -0600

    Create tests for edit and delete

---
+++ spec/features/merchants/edit_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Update Merchants" do
+  describe "As a visitor" do
+    describe "When I visit the merchant show page" do
+      it "I can update a Merchant" do
+        merchant_1 = Merchant.create(
+          name: "Knees Weak",
+          address: "1 Oppurtunity Way",
+          city: "Denver",
+          state: "Colorado",
+          zip: 80229
+        )
+
+        visit "/merchants/#{merchant_1.id}"
+
+        click_link 'Edit'
+
+        expect(current_path).to eq("/merchants/#{merchant_1.id}/edit")
+
+        fill_in "merchant[name]", with: "Mom's Spaghetti"
+        fill_in "merchant[address]", with: "1 Moment Way"
+        fill_in "merchant[city]", with: "Las Vegas"
+        fill_in "merchant[state]", with: "Nevada"
+        fill_in "merchant[zip]", with: 80003
+        click_on 'Update Merchant'
+
+        expect(current_path).to eq("/merchants/#{merchant_1.id}")
+        within("#merchant-#{merchant_1.id}") do
+          expect(page).to have_content("Mom's Spaghetti")
+          expect(page).to have_content("1 Moment Way")
+          expect(page).to have_content("Las Vegas")
+          expect(page).to have_content("Nevada")
+          expect(page).to have_content(80003)
+        end
+      end
+    end
+  end
+end
