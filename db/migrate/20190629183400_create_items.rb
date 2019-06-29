b1e52a979d2fec295c4f3ef0df423205a0e44c59
Sejin Kim <froydroyce@gmail.com>
2019-06-29 13:11:13 -0600

    Crate table for items

---
+++ db/migrate/20190629183400_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.integer :price
+      t.string :image
+      t.boolean :active
+      t.integer :inventory
+      t.string :merchant
+
+      t.timestamps
+    end
+  end
+end
