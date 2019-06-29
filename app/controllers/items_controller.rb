7c457de1598115292b7615ec5321a357da3264f4
Jori Peterson <joripeterson@gmail.com>
2019-06-29 15:39:50 -0600

    Add user 10 update item and test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,17 @@ class ItemsController < ApplicationController
     redirect_to "/merchants/#{merchant.id}/items"
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    item.save
+    redirect_to item_path(item)
+  end
+
   private
   def item_params
     params.permit(:name, :description, :price, :image, :active_status, :inventory)
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/merchants', to: 'merchants#index'
-  # get '/merchants/new', to: 'merchants#new'
-  # post '/merchants', to: 'merchants#create'
-  # get '/merchants/:id', to: 'merchants#show'
-  # get '/merchants/:id/edit', to: 'merchants#edit'
-  # patch '/merchants/:id', to: 'merchants#update'
-  # delete '/merchants/:id', to: 'merchants#destroy'
-  resources :items, only: [:index, :show]
+  resources :items, only: [:index, :show, :edit, :update]
   resources :merchants do
     resources :items, only: [:new, :create]
   end
 
   get '/merchants/:merchant_id/items', to: 'merchants#items'
-  # get '/merchants/:merchant_id/items/new', to: 'items#new'
-  # post '/merchants/:merchant_id/items', to: 'items#create'
 
-  # get '/items', to: 'items#index'
-  # get '/items/:id', to: 'items#show'
 end
