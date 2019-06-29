6bd546bf605ce4ca5ade725a279a2c740b81b0fc
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-28 20:56:18 -0600

    Remove unused image test

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "Items Index" do
   describe "As a visitor" do
     it "I see each item and its info in the system" do
       merchant_1 = Merchant.create(name: "What Ales You")
-      # item_1 = Item.create(name: "Galaxy Hops", description: "Huge hop oil content with pungent citrus and passion fruit flavors.", price: 36.99, image: "https://morebeer-web-8-pavinthewaysoftw.netdna-ssl.com/product_image/morebeer/500x500/22454.png", active: true, inventory: 28, merchant_name: merchant_1.name, merchant_id: merchant_1.id)
       item_1 = merchant_1.items.create(name: "Galaxy Hops", description: "Huge hop oil content with pungent citrus and passion fruit flavors.", price: 36.99, image: "https://morebeer-web-8-pavinthewaysoftw.netdna-ssl.com/product_image/morebeer/500x500/22454.png", active: true, inventory: 28)
 
       visit '/items'
@@ -12,7 +11,6 @@ RSpec.describe "Items Index" do
       expect(page).to have_content(item_1.name)
       expect(page).to have_content(item_1.description)
       expect(page).to have_content(item_1.price)
-      # expect(page).to have_content(item_1.image)
       expect(page).to have_css("img[src*='#{item_1.image}']")
       expect(page).to have_content("In Stock")
       expect(page).to have_content(item_1.inventory)
