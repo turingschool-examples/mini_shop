5128994ac756a3521e902f80b4b1813dcd354140
Stella Mainar <mainars09@alumni.hanover.edu>
2019-06-29 14:23:02 -0600

    Add updated schema after Items table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190626220842) do
+ActiveRecord::Schema.define(version: 20190629194258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.string "image"
+    t.boolean "active", default: true
+    t.integer "inventory"
+    t.bigint "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
@@ -25,4 +38,5 @@ ActiveRecord::Schema.define(version: 20190626220842) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "merchants"
 end
