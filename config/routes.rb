5da4fbf25ca3a52c4e33a70a6f2942e7374c3184
Jori Peterson <joripeterson@gmail.com>
2019-06-29 17:58:39 -0600

    Add navbar with merchant and item idex

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items, only: [:index, :show, :edit, :update]
+
   resources :merchants do
     resources :items, only: [:new, :create]
   end
