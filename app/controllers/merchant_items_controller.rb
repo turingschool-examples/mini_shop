76bd636049592c6cfb49f69475b8d6fb277d98fa
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-28 22:19:07 -0600

    Add actions to merchant items controller for new item form and create item

--- app/controllers/merchant_items_controller.rb
+++ app/controllers/merchant_items_controller.rb
@@ -4,5 +4,22 @@ class MerchantItemsController < ApplicationController
     @items = Merchant.find(params[:id]).items
     @merchant = Merchant.find(params[:id])
   end
-  
+
+  def new
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:id])
+    @merchant.items.create!(item_params)
+
+    redirect_to "/merchants/#{@merchant.id}/items"
+  end
+
+  private
+  def item_params
+    params.permit(:name, :price, :description, :image, :inventory)
+  end
+
+
 end
