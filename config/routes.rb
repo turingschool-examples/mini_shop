a0f3ef6ccc74b27464438ba5ce7616e9e87819ba
Stella Mainar <mainars09@alumni.hanover.edu>
2019-06-29 16:43:24 -0600

    Add route for Items Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   delete "/merchants/:id", to: "merchants#destroy"
 
   get "/items", to: "items#index"
+  get "/items/:id", to: "items#show"
 
   get "/merchants/:merchant_id/items", to: "merchants/items#index"
 end
