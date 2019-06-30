dbc6f189bcb6aade2d3ff5a20eabcf86501be6c3
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-06-30 17:40:12 -0600

    Refactor merchant items index test

--- spec/features/items/merchant_items_index_spec.rb
+++ spec/features/items/merchant_items_index_spec.rb
@@ -9,20 +9,13 @@ RSpec.describe "merchant items index"
 
     item_1 = Item.create!(name: "Tomahawk", description: "A hardened, high-carbon steel head makes this Tomahawk a great camp tool. The handle is hickory and 19 inches in length. This Tomahawk has great heft and balance great for target practice or competition.", price: 19.99, image: "<image of tomahawk here>", status: "active", inventory: 3, merchant_id: merchant_1.id )
 
-
-
-
     visit "/merchants/#{merchant_1.id}/items"
-
-
-
-
 
     expect(page).to have_content("Merchant: #{merchant_1.name}")
     expect(page).to have_content("Item: #{item_1.name}")
     expect(page).to have_content("Price: #{item_1.price}")
     expect(page).to have_content(item_1.image)
-    expect(page).to have_content("Status #{item_1.status}")
-    expect(page).to have_content("Inventory #{item_1.inventory}")
+    expect(page).to have_content("Status: #{item_1.status}")
+    expect(page).to have_content("Inventory: #{item_1.inventory}")
   end
 end
