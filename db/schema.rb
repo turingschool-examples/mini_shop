e7a772f707c61d6410c6ce5b2d64af290a55d462
Sejin Kim <froydroyce@gmail.com>
2019-06-30 16:14:39 -0600

    Recreate db to have correct relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190629202850) do
+ActiveRecord::Schema.define(version: 20190630191951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20190629202850) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.integer "price"
+    t.float "price"
     t.string "image"
     t.boolean "active"
     t.integer "inventory"
