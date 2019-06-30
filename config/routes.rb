fbf37ef06764b1f0483c8b8e0cd84c975d0b85b7
Sejin Kim <froydroyce@gmail.com>
2019-06-30 16:19:04 -0600

    Edit item resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :merchants
-  resources :items, only: [:index, :show]
+  resources :items, only: [ :index, :show, :edit, :update]
 
 
   resources :merchants do
-    resources :items
+    resources :items, only: [:index, :new, :create]
   end
 end
