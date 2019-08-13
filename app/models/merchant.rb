7e4f1fd10c4fd8960f817b5e72b97ab93fa5f6cd
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-12 22:39:05 -0600

    Create merchant model

---
+++ app/models/merchant.rb
@@ -0,0 +1,30 @@
+class Merchant < ApplicationRecord
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def destroy
+
+  end
+end
