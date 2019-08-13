c52975853b3c87e6988c4d646d71302841c47b36
Corina Allen <StarPerfect@gmail.com>
2019-08-13 15:23:17 -0600

    User Story 2 complete

---
+++ spec/features/merchants/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'merchants index page', type: :feature do
+  before :each do
+    @merchant_1 = Merchant.create(name: 'Some Store')
+  end
+
+  it 'shows all merchants' do
+    visit '/merchants'
+
+    expect(page).to have_content(@merchant_1.name)
+  end
+end
