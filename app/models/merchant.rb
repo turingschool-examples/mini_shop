ffc7fe25298209267c135dc551f85473e4a3a58b
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-12 22:41:28 -0600

    Remove controller methods from model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,30 +1,3 @@
 class Merchant < ApplicationRecord
 
-  def index
-    @merchants = Merchant.all
-  end
-
-  def show
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def new
-
-  end
-
-  def destroy
-
-  end
 end
