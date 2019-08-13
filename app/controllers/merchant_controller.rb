7bf5c4c60defad2bbeb5eedfc97b4ba8b5626d7c
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-12 22:41:58 -0600

    Create methods for model controller

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -1,3 +1,30 @@
 class MerchantController < ApplicationController
+  def index
+    @merchants = Merchant.all
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def new
+
+  end
+
+  def destroy
+
+  end
 
 end
