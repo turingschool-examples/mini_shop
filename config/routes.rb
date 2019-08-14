8b52d1754b676e7e897f16227952e8db6eed7780
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-14 13:28:50 -0600

    Add routes for editing and updating a merchant

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/merchants/new', to: 'merchants#new', as: :new_merchant
   post '/merchants', to: 'merchants#create'
   get '/merchants/:id', to: 'merchants#show', as: :merchant
+  get '/merchants/:id/edit', to: 'merchants#edit', as: :edit_merchant
+  patch '/merchants/:id/edit', to: 'merchants#update'
 
 end
