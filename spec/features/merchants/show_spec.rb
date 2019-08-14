e5b27655ce5409823837f897a97254dac144df4e
Corina Allen <StarPerfect@gmail.com>
2019-08-13 19:04:01 -0600

    Features/Merchant/show_spec test written

---
+++ spec/features/merchants/show_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'merchant show page', type: :feature do
+  before :each do
+    @merchant_1 = Merchant.create(name: 'Some Store', address: '123 Merchant Lane', city: 'Spend City', state: 'NY', zip: 00011)
+  end
+
+  it 'show merchant details' do
+    visit "/merchants/#{@merchant_1.id}"
+
+    expect(page).to have_content(@merchant_1.name)
+    expect(page).to have_content(@merchant_1.address)
+    expect(page).to have_content(@merchant_1.city)
+    expect(page).to have_content(@merchant_1.state)
+    expect(page).to have_content(@merchant_1.zip)
+  end
+end
