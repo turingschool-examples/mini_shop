aef09d78c15f1f7294ed1b24196de5ec598d688f
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-14 13:02:39 -0600

    Complete user story four

--- spec/features/merchants/new_spec.rb
+++ spec/features/merchants/new_spec.rb
@@ -5,12 +5,22 @@ RSpec.describe "merchants creation page", type: :feature do
     merchant_1 = Merchant.create(name: "Banana Bazaar", address: "123 Fruit Lane", city: "Niceville", state: "Florida", zip: 32578)
     merchant_2 = Merchant.create(name: "Cherry Corner", address: "456 Basket Cir", city: "Limon", state: "Colorado", zip: 80828)
 
-    visit "/merchants/new"
+    visit "/merchants"
 
-    fill_in "merchant_name", with: merchant_1.name
-    fill_in "merchant_address", with: merchant_1.address
-    fill_in "merchant_city", with: merchant_1.city
-    fill_in "merchant_state", with: merchant_1.state
-    fill_in "merchant_zip", with: merchant_1.zip
+    click_on "New Merchant"
+
+    expect(current_path).to eq("/merchants/new")
+
+    fill_in :name, with: "Mango Mall"
+    fill_in :address, with: "789 Palm Tree Dr"
+    fill_in :city, with: "San Diego"
+    fill_in :state, with: merchant_1.state
+    fill_in :zip, with: merchant_1.zip
+
+    click_on "Create Merchant"
+
+    # new_merchant = Merchant.last
+    expect(current_path).to eq("/merchants")
+    expect(page).to have_content("Mango Mall")
   end
 end
