719bd1f23809f30ccb563615e54e656d2b2efce8
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-14 12:19:45 -0600

    Add basic content test for merchant creation

--- spec/features/merchants/new_spec.rb
+++ spec/features/merchants/new_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 describe "merchant creation page" do
+  it "has a form with merchant information" do
+    visit "/merchants/new"
+
+    expect(page).to have_content("Merchant name:")
+    expect(page).to have_content("Merchant address:")
+    expect(page).to have_content("Merchant city:")
+    expect(page).to have_content("Merchant state:")
+    expect(page).to have_content("Merchant zip code:")
+  end
 
 end
