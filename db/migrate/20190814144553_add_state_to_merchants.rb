ce91ad8a9556d0d185c8f8dfe43213ac8e5f71a9
Nancy Lee <srlee713@gmail.com>
2019-08-14 17:03:24 -0600

    Reset database

--- db/migrate/20190814144553_add_state_to_merchants.rb
+++
@@ -1,5 +0,0 @@
-class AddStateToMerchants < ActiveRecord::Migration[5.1]
-  def change
-    add_column :merchants, :state, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190814144553) do
+ActiveRecord::Schema.define(version: 20190814224112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20190814144553) do
     t.string "name"
     t.string "address"
     t.string "city"
-    t.integer "zip"
     t.string "state"
+    t.integer "zip"
   end
 
 end
