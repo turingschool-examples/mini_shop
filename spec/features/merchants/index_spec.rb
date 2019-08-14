74a0116a38e429c92e6a209cc4906a6e80a02773
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-13 21:54:08 -0600

    Test create new merchant

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "merchant index page" do
+describe "merchant index page" do
   it "shows all merchants" do
     merchant_1 = Merchant.create(name: "Football Frenzy", address: "123 14th Circle", city: "Frederick", state: "CO", zip: 80530)
     merchant_2 = Merchant.create(name: "Gridiron Gear", address: "456 East Drive", city: "Longmont", state: "CO", zip: 80501)
@@ -10,4 +10,18 @@ RSpec.describe "merchant index page" do
     expect(page).to have_content(merchant_1.name)
     expect(page).to have_content(merchant_2.name)
   end
+
+  it "has a link to create a new merchant" do
+    merchant_1 = Merchant.create(name: "Football Frenzy", address: "123 14th Circle", city: "Frederick", state: "CO", zip: 80530)
+    merchant_2 = Merchant.create(name: "Gridiron Gear", address: "456 East Drive", city: "Longmont", state: "CO", zip: 80501)
+
+    visit "/merchants"
+
+    expect(page).to have_link("Create New Merchant")
+
+    click_link "Create New Merchant"
+
+    expect(current_path).to eq('/merchants/new')
+  end
+
 end
