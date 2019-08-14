bcae72c4298594b3d8215018590a74927055f3ec
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-14 13:30:02 -0600

    Create method to update and save the new merchant information

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -21,7 +21,10 @@ class MerchantsController < ApplicationController
   end
 
   def update
-    
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+    @merchant.save
+    redirect_to "/merchants"
   end
 
   def destroy
