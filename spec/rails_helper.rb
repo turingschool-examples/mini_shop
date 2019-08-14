10e890da440a614af902ccf9b308141183312195
Fenton Taylor <fentontaylor@gmail.com>
2019-08-14 15:31:00 -0600

    Add model test for merchant, validates presence of attributes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,6 @@
-require 'pry'
 require 'simplecov'
 SimpleCov.start
+require 'pry'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
@@ -62,4 +62,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
