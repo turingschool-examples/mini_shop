f35c82f954fb0e5f36f1db49572f29b62e4fd124
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-13 21:14:40 -0600

    Add validation tests for merchant

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe "merchant index page" do
   it "shows all merchants" do
-    merchant_1 = Merchant.create(name: "Fred-mart", address: "123 Fred Circle", city: "Frederick", state: "CO", zip: 80530)
-    merchant_2 = Merchant.create(name: "Lonnie's", address: "456 Lonnie Drive", city: "Longmont", state: "CO", zip: 80501)
+    merchant_1 = Merchant.create(name: "Football Frenzy", address: "123 14th Circle", city: "Frederick", state: "CO", zip: 80530)
+    merchant_2 = Merchant.create(name: "Gridiron Gear", address: "456 East Drive", city: "Longmont", state: "CO", zip: 80501)
 
     visit "/merchants"
 
