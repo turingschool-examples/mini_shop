efe67fcf10f02cb207669ca5b8f9c1154a2371da
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-15 16:31:11 -0600

    Add functionality to item index page so that image shows on page and add item to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+ Item.destroy_all
  Merchant.destroy_all
 
  merchant_1 = Merchant.create(name: "Banana Bazaar", address: "123 Fruit Lane", city: "Niceville", state: "Florida", zip: 32578)
  merchant_2 = Merchant.create(name: "Cherry Corner", address: "456 Basket Cir", city: "Limon", state: "Colorado", zip: 80828)
- item_1 = merchant_1.items.create(name: "Pineapple", description: "very large and very ripe", price: 3.23, image: "ripe_pineapple.jpg", active_status: true, inventory: 1)
+ item_1 = merchant_1.items.create(name: "Pineapple", description: "very large and very ripe", price: 3.23, image: "https://images-na.ssl-images-amazon.com/images/I/81qIPbnzqCL._SY679_.jpg", active_status: true, inventory: 1)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -3,15 +3,14 @@ require "rails_helper"
 RSpec.describe "items index page", type: :feature do
   it "user can see all items" do
     merchant_1 = Merchant.create(name: "Banana Bazaar", address: "123 Fruit Lane", city: "Niceville", state: "Florida", zip: 32578)
-    item_1 = merchant_1.items.create(name: "Pineapple", description: "very large and very ripe", price: 3.23, image: "ripe_pineapple.jpg", active_status: true, inventory: 1)
+    item_1 = merchant_1.items.create(name: "Pineapple", description: "very large and very ripe", price: 3.23, image: "https://images-na.ssl-images-amazon.com/images/I/81qIPbnzqCL._SY679_.jpg", active_status: true, inventory: 1)
 
     visit "/items"
 
     expect(page).to have_content(merchant_1.name)
     expect(page).to have_content(item_1.name)
     expect(page).to have_content(item_1.description)
-    expect(page).to have_content(item_1.price)
-    expect(page).to have_content(item_1.image)
+    expect(page).to have_css("img[src*='https://images-na.ssl-images-amazon.com/images/I/81qIPbnzqCL._SY679_.jpg']")
     expect(page).to have_content(item_1.active_status)
     expect(page).to have_content(item_1.inventory)
   end
