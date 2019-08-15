84f97e6c1aa73b054a66d8f497bfa19c370f9912
Nancy Lee <srlee713@gmail.com>
2019-08-15 14:11:26 -0600

    Complete User Story 5

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -15,6 +15,17 @@ class MerchantsController < ApplicationController
     redirect_to "/merchants"
   end
 
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+
+    redirect_to "/merchants/#{@merchant.id}"
+  end
+  
   private
   def merchant_params
     params.permit(:name, :address, :city, :state, :zip)
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/merchants/:id', to: 'merchants#show'
 
   post '/merchants', to: 'merchants#create'
+  get '/merchants/:id/edit', to: 'merchants#edit'
+  patch '/merchants/:id', to: 'merchants#update'
 end
