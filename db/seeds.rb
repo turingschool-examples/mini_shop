cb294e6d9b297a6f51859d1b27adbe17bc871c09
Corina Allen <StarPerfect@gmail.com>
2019-08-15 13:08:30 -0600

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+corina = Merchant.create(name: 'Corina', address: '1488 S Kenton', city: 'Aurora', state: 'CO', zip: 80012)
+corina = Merchant.create(name: 'Evette', address: '123 Street', city: 'Aurora', state: 'CO', zip: 80012)
+corina = Merchant.create(name: 'Fenton', address: '456 Lane', city: 'Aurora', state: 'CO', zip: 80012)
