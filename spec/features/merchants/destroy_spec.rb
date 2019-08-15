39d768dc550434b44825c9e5868d5e0ef2c11272
Fenton Taylor <fentontaylor@gmail.com>
2019-08-15 07:14:19 -0600

    Fix error with multiple merchants

--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe 'User clicks delete button on merchant show page' do
   it 'Destroys record of merchant' do
-    merchant_1 = Merchant.create!(name: 'Bob Ross Paints',
+    Item.destroy_all
+    Merchant.destroy_all
+    merchant_1 = Merchant.create(name: 'Bob Ross Paints',
                                 address: '2345 Happy Tree Place',
                                 city: 'Boulder',
                                 state: 'CO',
