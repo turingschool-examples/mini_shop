1bc1f6ec757517d09382a4f53dc29545e4152552
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-15 14:34:52 -0600

    Completes User Story 4

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,7 +3,21 @@ class MerchantsController < ApplicationController
     @merchants = ['Moose Munchies', 'Eh Plus School Supplies', 'Two-Four Liquors']
   end
 
+  def new
+    @merchant = Merchant.new
+  end
+
   def show
     @merchant = Merchant.find(params[:id])
+  end
+
+  def create
+    merchant = Merchant.create!(merchant_params)
+    redirect_to "/merchants"
+  end
+
+  private
+  def merchant_params
+    params.permit(:name, :address, :city, :state, :zip)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
   get '/merchants', to: 'merchants#index'
+  get '/merchants/new', to: 'merchants#new'
   get '/merchants/:id', to: 'merchants#show'
+  post '/merchants', to: 'merchants#create'
 end
