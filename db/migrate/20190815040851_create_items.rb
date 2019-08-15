b162659aca951c04302c987906610cf2b5f001c3
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-14 22:24:04 -0600

    Create table for items

---
+++ db/migrate/20190815040851_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.float :price
+      t.integer :inventory
+      t.string :image
+      t.string :status, default: "active"
+
+      t.references :merchant, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
