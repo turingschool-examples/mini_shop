94b23681e73b332c693be816d35c8a7080ac1ed2
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-14 22:44:55 -0600

    Update test for merchant item index

--- spec/features/merchant/item_index_spec.rb
+++ spec/features/merchant/item_index_spec.rb
@@ -4,22 +4,21 @@ RSpec.describe "Merchant Item Index Page", type: :feature do
   before :each do
     @meg = Merchant.create!(name: "Meg", address: "24 Bike Spoke Lane", city: "Denver", state: "CO", zip: "80237")
     @brian = Merchant.create!(name: "Brian", address: "549 Pike Peak Drive", city: "Greenville", state: "SC", zip: "23674")
-    @Kelly = Merchant.create!(name: "Kelly", address: "1 Lollipop Lane", city: "Candy Cane", state: "AZ", zip: "11265")
+    @kelly = Merchant.create!(name: "Kelly", address: "1 Lollipop Lane", city: "Candy Cane", state: "AZ", zip: "11265")
 
-    @bike = @meg.item.create!(name: "Bike", description: "It's a bike", price: 4000, image: "https://salsacycles.com/files/bikes/_small_image/2019_Marrakesh_Deore_Blue-uc-3.jpg", status: "active", inventory: 80)
-    @rope = @brian.item.create!(name: "Rope", description: "It's some rope", price: 250, image: "https://www.rei.com/media/product/898355", status: "active", inventory: 50)
-    @gummies = @kelly.item.create!(name: "Gummies", description: "It's a bag of gummies", price: 3, image: "https://sundayscaries.com/app/desktop/images/cbd-gummies-front.png", status: "active", inventory: 2000)
+    @bike = @meg.items.create!(name: "Bike", description: "It's a bike", price: 4000, image: "https://salsacycles.com/files/bikes/_small_image/2019_Marrakesh_Deore_Blue-uc-3.jpg", status: "active", inventory: 80)
+    @rope = @brian.items.create!(name: "Rope", description: "It's some rope", price: 250, image: "https://www.rei.com/media/product/898355", status: "active", inventory: 50)
+    @gummies = @kelly.items.create!(name: "Gummies", description: "It's a bag of gummies", price: 3, image: "https://sundayscaries.com/app/desktop/images/cbd-gummies-front.png", status: "active", inventory: 2000)
   end
   describe 'As a visitor' do
     it "When I visit '/merchants/:merchant_id/items', I see each Item that belongs
      to the Merchant with that merchant_id including the Item's attributes" do
       visit merchants_path
-
       click_link "Meg"
       expect(page).to have_link("Items")
 
       click_link "Items"
-      expect(current_path).to eq(merchant_items_path)
+      expect(current_path).to eq(merchant_items_path(@meg))
 
       expect(page).to have_content(@bike.name)
       expect(page).to have_content(@bike.price)
