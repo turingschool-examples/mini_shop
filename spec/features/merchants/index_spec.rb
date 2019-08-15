5bbb4de18d8608e9bcc2e3420898b8191fbdcfa7
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-15 12:53:30 -0600

    Add additional merchant for testing

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe "merchant index page", type: :feature do
 
     expect(page).to have_content(merchant_1.name)
     expect(page).to have_content(merchant_2.name)
+    expect(page).to have_content(merchant_3.name)
   end
 end
 
