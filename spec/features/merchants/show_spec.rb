d6d04c613f8652c1b3252acb785a31382c58c81f
Christopher Kelling <cj.kelling88@gmail.com>
2019-08-15 09:45:47 -0600

    Finish story 3

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe 'merchant show page' do
   before :each do
-    @merchant = Merchant.create(name: 'Dry Goods Store', address: '123 Main Street', city: 'Denver', state: 'CO', zip: '80021')
+    @merchant = Merchant.create(name: 'Dry Goods Store', address: '123 Main Street', city: 'Denver', state: 'CO', zip: 80021)
   end
 
   it 'should show merchant name, address, city, state, zip' do
-    visit '/merchants/:id'
+    visit "/merchants/#{@merchant.id}"
 
     expect(page).to have_content(@merchant.name)
     expect(page).to have_content(@merchant.address)
