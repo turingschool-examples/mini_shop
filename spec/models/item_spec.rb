a3692fe7be8c08cc092b85efac3a085f07da212c
Fenton Taylor <fentontaylor@gmail.com>
2019-08-14 20:50:21 -0600

    Add status method to item model, passes test

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,4 +13,26 @@ describe Item do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe 'instance methods' do
+    it 'status active or inactive for item.active' do
+      bob = Merchant.create( name: 'Bob Ross Paints', address: '2345 Happy Tree Place', city: 'Boulder', state: 'CO', zip: '80303')
+      brush = bob.items.create( name: 'Bushy Brush',
+                        description: 'A lovely bushy brush for brushing bushes.',
+                        price: 12.99,
+                        image: 'http://www.asianbrushpainter.com/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/h/a/happiness-highland-bull-horn-brush-black-goat-hair-1_27.jpg',
+                        active: true,
+                        inventory: 32)
+
+      paint = bob.items.create( name: 'Starter Paint Set',
+                        description: '24 beautiful natural colors for you to blend and stroke to your hearts content',
+                        price: 37.99,
+                        image: 'https://cdn.dick-blick.com/items/016/37/01637-0249-5-1-2ww-m.jpg',
+                        active: false,
+                        inventory: 17)
+
+      expect(brush.status).to eq('Active')
+      expect(paint.status).to eq('Inactive')
+    end
+  end
 end
