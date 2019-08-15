f80f889e6f096bff4b32221ca0fca7945325a175
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-08-14 22:57:13 -0600

    Add merchant and item model test file and update their respective model files

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe "merchants index page", type: :feature do
   it "user can see all merchants" do
-    merchant_1 = Merchant.create(name: "Banana Bazaar")
-    merchant_2 = Merchant.create(name: "Cherry Corner")
+    merchant_1 = Merchant.create(name: "Banana Bazaar", address: "123 Fruit Lane", city: "Niceville", state: "Florida", zip: 32578)
+    merchant_2 = Merchant.create(name: "Cherry Corner", address: "456 Basket Cir", city: "Limon", state: "Colorado", zip: 80828)
 
     visit "/merchants"
 
