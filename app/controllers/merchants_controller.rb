d0cc5af674ac5a4ed9e078ab43b5055456216a33
Scott Payton <spayton.c@gmail.com>
2019-08-15 10:20:18 -0600

    Add edit ERB and update controller and show view for merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -17,7 +17,19 @@ class MerchantsController < ApplicationController
    end
  end
 
-private
+ def edit
+  @merchant = Merchant.find(params[:id])
+end
+
+def update
+  @merchant = Merchant.find(params[:id])
+  if @merchant = Merchant.update(merchant_params)
+    redirect_to "/merchants/#{@merchant.first.id}"
+  end
+end
+
+
+private #makes everything blow this private methods
 
 def merchant_params
   params.permit(:name, :address, :city, :state, :zip)
