7031eb1edebe23885519cde155c03c921da2ab0c
Kelly Sandeson <kellysandeson@gmail.com>
2019-08-15 00:51:30 -0600

    Model Tests

---
+++ spec/models/merchants/merchant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Merchant do
+  describe 'Relationships' do
+    it {should have_many :items}
+  end
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip}
+  end
+end
