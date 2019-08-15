24290150daa976eb58ca8ca8fed9d766abe84ed5
Scott Payton <spayton.c@gmail.com>
2019-08-15 08:42:03 -0600

    Add routes for GET edit and POST update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/merchants', to: 'merchants#index'
-  get "/merchants/new", to: "merchants#new"
-  get '/merchants/:id', to: 'merchants#show'
-  post "/merchants", to: "merchants#create"
+  get '/merchants', to: 'merchants#index' #request to fetch  merchant index page
+  get "/merchants/new", to: "merchants#new" #request to fetch merchant new page
+  get '/merchants/:id', to: 'merchants#show' #request to fetch metchant show page
+  post "/merchants", to: "merchants#create" #request to post or create a new merchant
+  get "/merchants/:id/edit", to: "merchants#edit"#{request to fetch and display merchant edit page}
+  patch "/merchants/:id", to: "merchants#update" #request to change a part of a resource
 end
