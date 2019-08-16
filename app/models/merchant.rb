6d43546dabe7d38a4a8e389cce0fb216f8a09d98
Evette Telyas <etelyas@gmail.com>
2019-08-15 20:42:12 -0600

    added text output to availability

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
 
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def active_items(status)
+    items.select {|item| item.active == status}
+  end
 end
