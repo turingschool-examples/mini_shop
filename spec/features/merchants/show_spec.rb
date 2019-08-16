beeaa46a564724f8476c630b8816a17e932fa175
Leiya Kenney <leiya.kenney@gmail.com>
2019-08-16 09:04:28 -0600

    Completes user story 5

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -22,17 +22,22 @@ describe "Merchant Show Page" do
 
     click_link "Update Merchant"
     expect(current_path).to eq("/merchants/#{merchant.id}/edit")
-  end
 
-  # Then I am taken to '/merchants/:id/edit' where I  see a form to edit the merchant's data including:
-  # - name
-  # - address
-  # - city
-  # - state
-  # - zip
-  # When I fill out the form with updated information
-  # And I click the button to submit the form
-  # Then a `PATCH` request is sent to '/merchants/:id',
-  # the merchant's info is updated,
-  # and I am redirected to the Merchant's Show page where I see the merchant's updated info
+    name = "Poutine Parlour"
+    address = "514 Maple Syrup Dr"
+    city = "Montreal"
+    state = "MI"
+    zip = "47262"
+
+    fill_in "Name", with: name
+    fill_in "Address", with: address
+    fill_in "City", with: city
+    fill_in "State", with: state
+    fill_in "Zip", with: zip
+
+    click_on "Update Merchant"
+
+    expect(current_path).to eq("/merchants/#{merchant.id}")
+    expect(page).to have_content("514 Maple Syrup Dr")
+  end
 end
