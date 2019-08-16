ea516ddf36ed77e88191b039212970fa11a9de24
Fenton Taylor <fentontaylor@gmail.com>
2019-08-15 18:55:12 -0600

    Remove user story

--- spec/features/items/new_spec.rb
+++ spec/features/items/new_spec.rb
@@ -1,19 +1,3 @@
-# As a visitor
-# When I visit a Merchant Items Index page
-# Then I see a link to add a new item for that merchant
-# When I click the link
-# I am taken to '/merchants/:merchant_id/items/new' where I see a form to add a new item
-# When I fill in the form with the item's:
-# - name
-# - price
-# - description
-# - image
-# - inventory
-# Then a `POST` request is sent to '/merchants/:merchant_id/items',
-# a new item is created for that merchant,
-# that item has a status of 'active',
-# and I am redirected to the Merchant Items Index page where I see the new item
-
 require 'rails_helper'
 
 describe 'User visits merchant items index page' do
