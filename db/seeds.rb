83a8e2d281055e47a63a93bb2306193ade8e49c5
Corina Allen <StarPerfect@gmail.com>
2019-08-17 17:22:54 -0600

    Update Seed, add Destroy All

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Item.destroy_all
+Merchant.destroy_all
+
 corina = Merchant.create(name: 'Corina', address: '1488 S Kenton', city: 'Aurora', state: 'CO', zip: 80012)
 pink = Merchant.create(name: 'PINK', address: '3000 E 1st Ave', city: 'Denver', state: 'CO', zip: 80206)
-hoodie = Item.create(name: 'Hoodie', description: 'Black Pull Over Hoodie', price: 25, image: 'https://thenypost.files.wordpress.com/2018/03/shutterstock_450063892.jpg?quality=90&strip=all&w=618&h=410&crop=1', status: true, inventory: 11, merchant_id: @pink.id)
-hoodie = corina.items.create!(name: 'Hoodie', description: 'Black Pull Over Hoodie', price: 25, image: 'https://thenypost.files.wordpress.com/2018/03/shutterstock_450063892.jpg?quality=90&strip=all&w=618&h=410&crop=1', status: true, inventory: 11)
+hoodie = pink.items.create(name: 'Hoodie', description: 'Black Pull Over Hoodie', price: 25, image: 'https://thenypost.files.wordpress.com/2018/03/shutterstock_450063892.jpg?quality=90&strip=all&w=618&h=410&crop=1', status: true, inventory: 11)
