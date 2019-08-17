438f0a76dbe46da8fc1ad2a6c191fb0f06e96fbe
Fenton Taylor <fentontaylor@gmail.com>
2019-08-17 13:34:00 -0600

    Destroys all merchant items before destroying merchant

--- spec/features/merchants/destroy_spec.rb
+++ spec/features/merchants/destroy_spec.rb
@@ -7,6 +7,13 @@ describe 'User clicks delete button on merchant show page' do
                                 city: 'Boulder',
                                 state: 'CO',
                                 zip: '80303')
+    brush = merchant_1.items.create( name: 'Bushy Brush',
+                      description: 'A lovely bushy brush for brushing bushes.',
+                      price: 12.99,
+                      image: 'http://www.asianbrushpainter.com/media/catalog/product/cache/1/thumbnail/9df78eab33525d08d6e5fb8d27136e95/h/a/happiness-highland-bull-horn-brush-black-goat-hair-1_27.jpg',
+                      active: true,
+                      inventory: 32)
+
     merchant_2 = Merchant.create( name: 'Yum Yum Snacks',
                                   address: '123 Tasty Town Ave',
                                   city: 'Denver',
