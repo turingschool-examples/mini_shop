db815212ae3f68b7a956dc9ebd4bd2919250d584
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-17 17:05:11 -0600

    Error message added for sad path User Story 4

--- spec/features/merchants/new_spec.rb
+++ spec/features/merchants/new_spec.rb
@@ -2,29 +2,42 @@ require "rails_helper"
 
 RSpec.describe "create new merchant" do
 
-  it 'can create new merchant' do
-    visit "/merchants"
+  # it 'can create new merchant' do
+  #   visit "/merchants"
+  #
+  #   click_link "New Merchant"
+  #
+  #   expect(current_path).to eq("/merchants/new")
+  #
+  #   fill_in :name, with: "The Busy Business"
+  #   fill_in :address, with: "0123 West St"
+  #   fill_in :city, with: "Chicago"
+  #   fill_in :state, with: "IL"
+  #   fill_in :zip, with: "60603"
+  #
+  #   click_on "Submit"
+  #
+  #   new_merchant = Merchant.last
+  #
+  #   expect(current_path).to eq("/merchants")
+  #   expect(page).to have_content("The Busy Business")
+  # end
+  #
+  # it 'cannot create new merchant without a name' do
+  #   visit "/merchants/new"
+  #
+  #   click_on "Submit"
+  #
+  #   expect(page).to have_content("Merchant not created: Required information missing.")
+  #   expect(page).to have_button("Submit")
+  # end
 
-    click_link "New Merchant"
+  it 'cannot create new merchant without all merchant info' do
+    visit "/merchants/new"
 
-    expect(current_path).to eq("/merchants/new")
-
-    fill_in :name, with: "The Busy Business"
     fill_in :address, with: "0123 West St"
     fill_in :city, with: "Chicago"
-    fill_in :state, with: "IL"
     fill_in :zip, with: "60603"
-
-    click_on "Submit"
-
-    new_merchant = Merchant.last
-
-    expect(current_path).to eq("/merchants")
-    expect(page).to have_content("The Busy Business")
-  end
-
-  it 'cannot create new merchant without a name' do
-    visit "/merchants/new"
 
     click_on "Submit"
 
