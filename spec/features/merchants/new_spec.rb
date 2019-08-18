cdf12afeae9a2c654713325a40b7657e4e13009d
MackHalliday <mackenzie.halliday@gmail.com>
2019-08-17 21:39:49 -0600

    Removed spec testing for sad path merchant edit

--- spec/features/merchants/new_spec.rb
+++ spec/features/merchants/new_spec.rb
@@ -22,26 +22,4 @@ RSpec.describe "create new merchant" do
     expect(current_path).to eq("/merchants")
     expect(page).to have_content("The Busy Business")
   end
-
-  # it 'cannot create new merchant without any info' do
-  #   visit "/merchants/new"
-  #
-  #   click_on "Submit"
-  #
-  #   expect(page).to have_content("Merchant not created. The following fields were not entered or entered incorrectly: Name, Address, City, Zip, State.")
-  #   expect(page).to have_button("Submit")
-  # end
-
-  # it 'cannot create new merchant with only partial information' do
-  #   visit "/merchants/new"
-  #
-  #   fill_in :address, with: "0123 West St"
-  #   fill_in :city, with: "Chicago"
-  #   fill_in :zip, with: "60603"
-  #
-  #   click_on "Submit"
-  #
-  #   expect(page).to have_content("Merchant not created. The following fields were not entered or entered incorrectly: Name, State.")
-  #   expect(page).to have_button("Submit")
-  # end
 end
