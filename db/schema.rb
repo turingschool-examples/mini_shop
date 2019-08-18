ec9c962968df787de98c55748d621fd612cf6852
Scott Payton <spayton.c@gmail.com>
2019-08-17 18:45:22 -0600

    New DB schema for item merchant relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190814212209) do
+ActiveRecord::Schema.define(version: 20190816170149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.string "image"
+    t.boolean "status"
+    t.integer "inventory"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
@@ -21,6 +34,9 @@ ActiveRecord::Schema.define(version: 20190814212209) do
     t.string "city"
     t.string "state"
     t.string "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
+  add_foreign_key "items", "merchants"
 end
