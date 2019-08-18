a9007bcdb52c560e24124294f3c2f47e2641b9c2
Alec Wells <alec.wells47@gmail.com>
2019-08-18 17:10:38 -0600

    Add active model method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   validates_presence_of :name, :description, :price, :image, :inventory
+
+  def active?
+    if self.inventory > 0
+      self.active = true
+    else
+      self.active = false
+    end
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,6 @@ Item.destroy_all
 @merchant_1 = Merchant.create(name: "merchant_1", address: "123 Main", city: "Denver", state:"CO", zip:"80204")
 @merchant_2 = Merchant.create(name: "merchant_2", address: "789 Pacific", city: "Lakewood", state:"CO", zip:"80214")
 
-@item_1 = @merchant_1.items.create(name: "flylow chemical", description: "Mens ski pant", price: 325, image: "https://content.backcountry.com/images/items/1200/FLG/FLG0029/FER.jpg" , active: true, inventory: 12)
+@item_1 = @merchant_1.items.create(name: "flylow chemical", description: "Mens ski pant", price: 325, image: "https://content.backcountry.com/images/items/1200/FLG/FLG0029/FER.jpg" , active: true, inventory: 0)
 @item_2 = @merchant_1.items.create(name: "ON3P Jeffrey", description: "Ski", price: 699, image: "https://abetterski.com/assets/lg-gallery/on3p/skis/jeffrey-114/on3p-jeffrey-114.jpg" , active: true, inventory: 9)
 @item_2 = @merchant_2.items.create(name: "ON3P Jeffrey", description: "Big mountain ski", price: 699, image: "https://cdn.shopify.com/s/files/1/0033/4622/products/2020_Jeffrey116_Preview_165x1000x72px-01_2000x.png?v=1555779770" , active: true, inventory: 9,)
