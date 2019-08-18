b22d8e2c6edd9c9e4b0ecb8750852ec6d6ac7f21
Corina Allen <StarPerfect@gmail.com>
2019-08-17 19:07:11 -0600

    User Story 8 Completee

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
-  belongs_to :merchant 
+  belongs_to :merchant
+  # Trying to write class method to provide the Status field as Active or
+  # Inactive in view but kept as a boolean
+  # def self.status
+  #   if status = true
+  #     'Active'
+  #   else
+  #     'Inactive'
+  #   end
+  # end
 end
