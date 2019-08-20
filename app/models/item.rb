243250d0ba18f6b0fc7f7462148f2c7172347da0
Nancy Lee <srlee713@gmail.com>
2019-08-19 21:39:51 -0600

    Add validations to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  validates_presence_of :name, :description, :price, :image, :inventory
+  validates_inclusion_of :status, :in => [true, false]
 end
