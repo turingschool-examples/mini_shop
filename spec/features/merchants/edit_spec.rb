e020081395fe3a643140113750f0a5cf4c21b51a
Nancy Lee <srlee713@gmail.com>
2019-08-19 22:16:36 -0600

    Refactor merchant edit_spec

--- spec/features/merchants/edit_spec.rb
+++ spec/features/merchants/edit_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   describe 'When I visit the merchants show page' do
-    it 'I see a link to update a merchant' do
-      merchant_1 = Merchant.create!(name: "Cecilia Chapman", address: "711-2880 Nulla St.", city: "Mankato", state: "MS", zip: 96522)
+    before(:each) do
+      @merchant_1 = Merchant.create!(name: "Cecilia Chapman", address: "711-2880 Nulla St.", city: "Mankato", state: "MS", zip: 96522)
 
-      visit "/merchants/#{merchant_1.id}"
+      visit "/merchants/#{@merchant_1.id}"
+    end
 
+    it "I see the form with prepopulated fields" do
       expect(page).to have_link("Edit")
 
       click_link 'Edit'
 
-      expect(current_path).to eq("/merchants/#{merchant_1.id}/edit")
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}/edit")
+      expect(find_field('Name').value).to eq("Cecilia Chapman")
+      expect(find_field('Address').value).to eq("711-2880 Nulla St.")
+      expect(find_field('City').value).to eq("Mankato")
+      expect(find_field('State').value).to eq("MS")
+      expect(find_field('Zip').value).to eq("96522")
+    end
+
+
+    it 'I see a link to update a merchant' do
+      click_link 'Edit'
 
       fill_in :name, with: "Mark"
       fill_in :address, with: "4628 E 23rd Ave"
@@ -20,13 +32,18 @@ RSpec.describe 'As a visitor' do
       fill_in :zip, with: 80207
 
       click_button 'Update Merchant'
-      
-      expect(current_path).to eq("/merchants/#{merchant_1.id}")
+
+      expect(current_path).to eq("/merchants/#{@merchant_1.id}")
       expect(page).to have_content("Mark")
+      expect(page).to_not have_content("Cecilia Chapman")
       expect(page).to have_content("4628 E 23rd Ave")
+      expect(page).to_not have_content("711-2880 Nulla St.")
       expect(page).to have_content("Denver")
+      expect(page).to_not have_content("Mankato")
       expect(page).to have_content("CO")
+      expect(page).to_not have_content("MS")
       expect(page).to have_content(80207)
+      expect(page).to_not have_content(96522)
     end
   end
 end
