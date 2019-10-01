b06a777f94bbc6f2f32ba998b05c5795a6ed8671
Mary Lang <mcl86@cornell.edu>
2019-09-30 21:59:31 -0600

    Fix seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-merchant_1 = Merchant.create!(name: "Henry", address: "410 Bleeker Street", city: "Philadelphia", state: "PA", zip: "19107" )
-merchant_2 = Merchant.create!(name: "Olivia", address: "384 Spring Ave", city: "New York", state: "NY", zip: "11221" )
-merchant_3 = Merchant.create!(name: "Ava", address: "930 Sunrise Blvd", city: "San Diego", state: "CA", zip: "92103" )
+    merchant_1 = Merchant.create!(name: "Henry", address: "410 Bleeker Street", city: "Philadelphia", state: "PA", zip: "19107" )
+    merchant_2 = Merchant.create!(name: "Olivia", address: "384 Spring Ave", city: "New York", state: "NY", zip: "11221" )
+    merchant_3 = Merchant.create!(name: "Ava", address: "930 Sunrise Blvd", city: "San Diego", state: "CA", zip: "92103" )
