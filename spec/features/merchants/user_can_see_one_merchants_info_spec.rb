03166a7c18596e00ac7331789e7a8986e3708215
Alice Post <alicep2322@gmail.com>
2019-10-01 17:24:32 -0600

    Show merchant information

---
+++ spec/features/merchants/user_can_see_one_merchants_info_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "merchants show page", type: :feature do
+  it "can see merchant's information" do
+    merchant_1 = Merchant.create( name: "Corner Store",
+                                  address: "234 Yarp Dr.",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip: "80211")
+    merchant_2 = Merchant.create( name: "Store Store",
+                                  address: "567 Narp Dr.",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip: "80203")
+    merchant_3 = Merchant.create( name: "Your More Store",
+                                  address: "890 Aardvark Dr.",
+                                  city: "Denver",
+                                  state: "CO",
+                                  zip: "80201")
+
+    visit "/merchants/#{merchant_1.id}"
+    expect(page).to have_content(merchant_1.name)
+    expect(page).to have_content(merchant_1.address)
+    expect(page).to have_content(merchant_1.city)
+    expect(page).to have_content(merchant_1.state)
+    expect(page).to have_content(merchant_1.zip)
+
+    visit "/merchants/#{merchant_2.id}"
+    expect(page).to have_content(merchant_2.name)
+    expect(page).to have_content(merchant_2.address)
+    expect(page).to have_content(merchant_2.city)
+    expect(page).to have_content(merchant_2.state)
+    expect(page).to have_content(merchant_2.zip)
+
+    visit "/merchants/#{merchant_3.id}"
+    expect(page).to have_content(merchant_3.name)
+    expect(page).to have_content(merchant_3.address)
+    expect(page).to have_content(merchant_3.city)
+    expect(page).to have_content(merchant_3.state)
+    expect(page).to have_content(merchant_3.zip)
+
+  end
+end
