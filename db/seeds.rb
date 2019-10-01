27a415f23ab171212d27a4dacd3b4b0f06da088e
Graham Thompson <grwthomps@gmail.com>
2019-10-01 14:49:53 -0600

    Add 3 merchants to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+merchants = Merchant.create([{name: "Puppers R Us", address: "425 Dog Day Street", city: "Denver", state: "CO", zip: "80210" },
+                             {name: "Kitten Market", address: "818 Catastic Avenue", city: "Orlando", state: "FL", zip: "32810"},
+                             {name: "Owl City", address: "739 Hoot Boulevard", city: "New Orleans", state: "LA", zip: "70010"}])
