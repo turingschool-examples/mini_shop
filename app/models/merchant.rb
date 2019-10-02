866780b2433cb0c5b2d4fa36a05968d8e9146222
Alice Post <alicep2322@gmail.com>
2019-10-02 15:12:32 -0600

    Validate base attributes of Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,7 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
 end
