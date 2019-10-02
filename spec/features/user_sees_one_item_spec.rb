b03291e78690435266fa5da697a1f104cb46f20b
Mary Lang <mcl86@cornell.edu>
2019-10-01 21:02:03 -0600

    Add test for item show

---
+++ spec/features/user_sees_one_item_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "user sees one item" do
+  describe "they visit /items/:id" do
+    it "displays item information" do
+      merchant_1 = Merchant.create!(name: "Thai Tanic", address: "410 Bleeker Street", city: "Philadelphia", state: "PA", zip: "19107" )
+      item_1 = Item.create!(name: "Panang Curry", description: "Sweet and spicy", price: 12.50, image: 'http://www.aspicyperspective.com/wp-content/uploads/2017/09/The-Best-Thai-Panang-Chicken-Curry-11.jpg', status: "active", inventory: 12, merchant_id: merchant_1.id)
+
+      visit '/items'
+
+      click_link item_1.name
+
+      expect(page).to have_content(item_1.name)
+      expect(page).to have_content(item_1.description)
+      expect(page).to have_content(item_1.price)
+      expect(page).to have_content(item_1.image)
+      expect(page).to have_content(item_1.status)
+      expect(page).to have_content(item_1.inventory)
+      expect(page).to have_content(item_1.merchant_id)
+
+    end
+  end
+end
