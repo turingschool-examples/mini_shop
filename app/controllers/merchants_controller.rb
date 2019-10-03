b48f611e5b89a618d22ed5c11af9667569dc5da2
Rachel Lew <rlew07@gmail.com>
2019-10-03 15:40:55 -0600

    Add new method

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,9 +1,13 @@
 class MerchantsController < ApplicationController
+
   def index
     @merchants = Merchant.all
   end
 
   def show
     @merchant = Merchant.find(params[:id])
+  end
+
+  def new
   end
 end
