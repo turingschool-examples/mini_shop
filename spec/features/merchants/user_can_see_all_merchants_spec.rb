cdb8ee80f96ca26c2f6c543827f2c6ffc38264c3
Laura Schulz <lrs8810@gmail.com>
2019-10-03 08:42:25 -0600

    Update spec for merchant index

--- spec/features/merchants/user_can_see_all_merchants_spec.rb
+++ spec/features/merchants/user_can_see_all_merchants_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "merchants index page", type: :feature do
   it "can see all merchants in the system" do
-    merchant_1 = Merchant.create(name: "Seller of the best things")
-    merchant_2 = Merchant.create(name: "Fun Seller")
+    merchant_1 = Merchant.create(name: "Sedona", address: '123 Main Street', city: 'Denver', state: 'CO', zip: 80218)
+    merchant_2 = Merchant.create(name: "Fun Fitness", address: '75 Springride Meadows', city: 'Los Angeles', state: 'CA', zip: 90040)
 
   visit "/merchants"
 
-  expect(page).to have_content(merchant_1.name)
-  expect(page).to have_content(merchant_2.name)
+  expect(page).to have_link(merchant_1.name)
+  expect(page).to have_link(merchant_2.name)
   end
 end
