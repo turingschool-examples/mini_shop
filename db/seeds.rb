8ea4e18dbd3212ba0c9d5371d96ddee200aa8420
Scott Regenthal <scottregenthal@gmail.com>
2019-10-02 18:44:23 -0600

    Populate seeds file with merchant names

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+merchant_1 = Merchant.create(name: "Oomba")
+merchant_2 = Merchant.create(name: "Kozmo")
