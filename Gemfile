8dd21644029ff19efd2aab64ccd47efae3848026
Laura Schulz <lrs8810@gmail.com>
2019-10-03 11:39:46 -0600

    Add assertion not to have to spec for one merchant

--- Gemfile
+++ Gemfile
@@ -27,11 +27,6 @@ end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
-  gem 'rspec-rails'
-  gem 'capybara'
-  gem 'launchy'
-  gem 'pry'
-  gem 'simplecov'
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
 end
--- spec/features/merchants/user_can_see_specific_merchant_details_spec.rb
+++ spec/features/merchants/user_can_see_specific_merchant_details_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe "individual merchant detail page", type: :feature do
   it "can see the details for a specific merchant" do
     merchant_1 = Merchant.create(name: "Sedona", address: "123 Main Street", city: "Denver", state: "CO", zip: "80218")
+    merchant_2 = Merchant.create(name: "Fun Fitness", address: "Another street", city: "Denver", state: "CO", zip: "80218")
 
-    visit merchants_path
+    visit "/merchants/#{merchant_1.id}"
 
     expect(page).to have_content(merchant_1.name)
     expect(page).to have_content(merchant_1.address)
     expect(page).to have_content(merchant_1.city)
     expect(page).to have_content(merchant_1.state)
     expect(page).to have_content(merchant_1.zip)
+    expect(page).not_to have_content(merchant_2.name)
   end
 end
