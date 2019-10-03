97e4318b9bf54fc0cc57e6fa83ab40dcd95c7f00
Scott Regenthal <scottregenthal@gmail.com>
2019-10-03 15:18:18 -0600

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Merchant.destroy_all
-merchant_1 = Merchant.create(name: "Oomba")
-merchant_2 = Merchant.create(name: "Kozmo")
+
+merchant_1 = Merchant.create(name:    "Oomba",
+                             address: "9840 Irvine Center Drive",
+                             city:    "Irvine",
+                             state:   "CA",
+                             zip:     92618)
+merchant_2 = Merchant.create(name:    "Kozmo",
+                             address: "80 Broad Street 14th Floor",
+                             city:    "New York",
+                             state:   "NY",
+                             zip:     10004)
