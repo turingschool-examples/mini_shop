49645e75811a78203b22777d9d1f95abaff05c4b
John Travers <johnk.travers@gmail.com>
2019-10-02 23:27:39 -0600

    Remove unnecessary comments

--- spec/features/merchants/item_creation_spec.rb
+++ spec/features/merchants/item_creation_spec.rb
@@ -1,21 +1,5 @@
 require 'rails_helper'
 
-# As a visitor
-# When I visit a Merchant Items Index page
-# Then I see a link to add a new item for that merchant
-# When I click the link
-# I am taken to '/merchants/:merchant_id/items/new' where I see a form to add a new item
-# When I fill in the form with the item's:
-# - name
-# - price
-# - description
-# - image
-# - inventory
-# Then a `POST` request is sent to '/merchants/:merchant_id/items',
-# a new item is created for that merchant,
-# that item has a status of 'active',
-# and I am redirected to the Merchant Items Index page where I see the new item
-
 RSpec.describe "merchant item creation" do
 
   before :each do
