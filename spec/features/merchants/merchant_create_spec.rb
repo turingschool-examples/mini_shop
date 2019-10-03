4ef6513f44eb15333a21f529a01e2d46a3b5ffb0
Brian Bower <brian@brianbower.com>
2019-10-03 13:46:46 -0600

    Change forms and links to rails format

--- spec/features/merchants/merchant_create_spec.rb
+++ spec/features/merchants/merchant_create_spec.rb
@@ -1,40 +1,26 @@
 require 'rails_helper'
 
 describe "merchant create page", type: :feature do
-  before :each do
-    @merchant = Merchant.create( name: "Chesters Collectables",
-                                  address: "123 Fake St",
-                                  city: "Awesomeville",
-                                  state: "Montana",
-                                  zip: 59001,
-    )
-  end
 
   it "can create a new merchant with a name, address, city, state, zip" do
     visit '/merchants'
     click_link('Create New Merchant')
-    # expect(current_path).to eql('/merchants/new') QUESTION FOR CODE REVIEW - How do I test this if it goes below?
-    fill_in 'merchant[name]', with: '@merchant.name'
-    fill_in('merchant[address]', :with => '@merchant.address')
-    fill_in 'merchant[city]', with: '@merchant.city'
-    fill_in 'merchant[state]', with: '@merchant.state'
-    fill_in 'merchant[zip]', with: @merchant.zip
-    click_button('submit')
-    # expect(current_path).to eql('/merchants')
-  end
+    expect(current_path).to eql('/merchants/new')
+    fill_in 'Name', with: 'Chesters Place'
+    fill_in('Address', :with => '123 fake st')
+    fill_in 'City', with: 'denver'
+    fill_in 'State', with: 'co'
+    fill_in 'Zip', with: 80111
+    click_on('submit')
+    expect(current_path).to eql('/merchants')
+    expect(page).to have_content('Chesters Place')
 
-  it "shows the merchant on the index page after creation" do
-    visit "merchants"
-    expect(page).to have_content(@merchant.name)
-  end
-
-  it "shows the merchants info on the merchants page after creation" do
-    visit "merchants/#{@merchant.id}"
-    expect(page).to have_content(@merchant.name)
-    expect(page).to have_content(@merchant.address)
-    expect(page).to have_content(@merchant.city)
-    expect(page).to have_content(@merchant.state)
-    expect(page).to have_content(@merchant.zip)
+    visit "merchants/#{Merchant.last.id}"
+    expect(page).to have_content('Chesters Place')
+    expect(page).to have_content('123 fake st')
+    expect(page).to have_content('denver')
+    expect(page).to have_content('co')
+    expect(page).to have_content(80111)
   end
 
 end
