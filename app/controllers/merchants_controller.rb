fe97cd152aeb012a39463803fb816df6ad5c5dd0
Alice Post <alicep2322@gmail.com>
2019-10-02 18:24:55 -0600

    Start test route view and controller for new merchant

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -3,6 +3,19 @@ class MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
+  def new
+  end
+
+#   def create
+#     binding.pry
+#   merchant = Merchant.new({
+#     name: params[:task][:title],
+#     description: params[:task][:description]
+#     })
+#   merchant.save
+#   redirect_to '/merchants'
+# end
+
   def show
     @merchant = Merchant.find(params[:id])
   end
