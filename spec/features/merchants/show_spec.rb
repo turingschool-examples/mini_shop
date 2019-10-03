8adb7db1b8f739321a3e758e5e1ed5aac944cdc8
John Travers <johnk.travers@gmail.com>
2019-10-03 12:56:06 -0600

    Add link to merchant item index from merchant show page

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,22 +2,32 @@ require 'rails_helper'
 
 RSpec.describe "merchants show page", type: :feature do
 
-  it "can see name address city state zip" do
-    merchant = Merchant.create(
+  before :each do
+    @merchant = Merchant.create(
       name: 'Once and Floral',
       address: '76 Poinsetta Street',
       city: 'Johnstown',
       state: 'AK',
       zip: 94591
     )
+  end
 
-    visit "/merchants/#{merchant.id}"
+  it "can see name address city state zip" do
+    visit "/merchants/#{@merchant.id}"
 
-    expect(page).to have_content(merchant.name)
-    expect(page).to have_content("Address: #{merchant.address}")
-    expect(page).to have_content("City: #{merchant.city}")
-    expect(page).to have_content("State: #{merchant.state}")
-    expect(page).to have_content("Zip Code: #{merchant.zip}")
+    expect(page).to have_content(@merchant.name)
+    expect(page).to have_content("Address: #{@merchant.address}")
+    expect(page).to have_content("City: #{@merchant.city}")
+    expect(page).to have_content("State: #{@merchant.state}")
+    expect(page).to have_content("Zip Code: #{@merchant.zip}")
+  end
+
+  it "can click on items sold to redirect to merchant items index" do
+    visit "/merchants/#{@merchant.id}"
+    expect(page).to have_link('Items Sold')
+
+    click_link 'Items Sold'
+    expect(current_path).to eq("/merchants/#{@merchant.id}/items")
   end
 
 end
