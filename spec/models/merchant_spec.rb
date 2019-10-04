e61e1e5c14ee5c50b6d82fa2615469d38c153e72
Jon Patterson <JonPatt92@gmail.com>
2019-10-04 16:49:34 -0600

    Build item and merchant model tests

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Merchant, type: :model do
+  #validations
+  describe "validations" do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :address}
+    it { should validate_presence_of :city}
+    it { should validate_presence_of :state}
+    it { should validate_presence_of :zip}
+  end
+
+  #relationships
+  describe "relationships" do
+    it {should have_many :items}
+  end
+end
