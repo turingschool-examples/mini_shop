c199bc53b2e67dc537b9c3edbef4c2fb7a7b990c
Graham Thompson <grwthomps@gmail.com>
2019-10-03 18:16:46 -0600

    Add route for '/items' to index

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get '/merchants', to: 'merchants#index'
   get '/merchants/new', to: 'merchants#new'
   get '/merchants/:id', to: 'merchants#show'
-  post '/merchants', to: 'merchants#create'
   get '/merchants/:id/edit', to: 'merchants#edit'
+  get '/items', to: 'items#index'
+  post '/merchants', to: 'merchants#create'
   patch '/merchants/:id', to: 'merchants#update'
   delete '/merchants/:id', to: 'merchants#destroy'
 end
