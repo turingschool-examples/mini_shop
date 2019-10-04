9221e527fb04e170daf972a1d2e69127c44f1bf0
Ryan Hantak <rhantak@gmail.com>
2019-10-03 18:23:57 -0600

    Add patch functionality for merchants

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/merchants', to: 'merchants#index'
   get '/merchants/new', to: 'merchants#new'
   get '/merchants/:id', to: 'merchants#show'
+  get '/merchants/:id/update', to: 'merchants#update'
   post '/merchants', to: 'merchants#create'
+  patch '/merchants/:id', to: 'merchants#change_info'
 end
