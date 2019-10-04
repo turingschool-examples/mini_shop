c07cc8cdd5b6447b63a884448ef2f0e2a5bba159
Michael Cooper <macoop2363@gmail.com>
2019-10-04 12:36:44 -0600

    Added item model

---
+++ app/models/item.rb
@@ -0,0 +1,9 @@
+class Item < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :price
+  validates_presence_of :image
+  validates_presence_of :active
+  validates_presence_of :inventory
+  validates_presence_of :merchant_id
+end
