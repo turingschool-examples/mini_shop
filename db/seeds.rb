c1067d3934c82f078f75b736827f3f88099657fb
Graham Thompson <grwthomps@gmail.com>
2019-10-04 17:56:04 -0600

    Change image path to remote url in seeds due to Heroku not loading them

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,41 +27,41 @@ merchant_3 = Merchant.create(name: "Owl City",
 merchant_1.items.create(name: "Dog Bowl",
                         description: "5\" stainless steel dog bowl",
                         price: 35.99,
-                        image: "/assets/dog_bowl.jpg",
+                        image: "https://i.imgur.com/3H1e3k7.jpg",
                         status: "active",
                         inventory: 5)
 
 merchant_1.items.create(name: "Dog Bed",
                         description: "Luxury memory foam dog bed for your pupper (dog not included!)",
                         price: 80,
-                        image: "/assets/dog_bed.jpg",
+                        image: "https://i.imgur.com/D8a4HLN.jpg",
                         status: "active",
                         inventory: 7)
 
 merchant_2.items.create(name: "Cat Carrier",
                         description: "Spacious carrier for your feline friend. It will feel like a hotel for your cat! Three zippable flaps.",
                         price: 27.42,
-                        image: "/assets/cat_carrier.jpg",
+                        image: "https://i.imgur.com/1tX0wB0.jpg",
                         status: "active",
                         inventory: 3)
 
 merchant_2.items.create(name: "Cat Post",
                         description: "Scratching and resting post combo, two for one! 27\" tall.",
                         price: 25,
-                        image: "/assets/cat_post.jpg",
+                        image: "https://i.imgur.com/vim9kYM.jpg",
                         status: "active",
                         inventory: 9)
 
 merchant_3.items.create(name: "Leather Falconry Gloves",
                         description: "Pristine leather falconry gloves, one size fits all. Superior quality and build. Lifetime warranty.",
                         price: 149.99,
-                        image: "/assets/falconry_gloves.jpg",
+                        image: "https://i.imgur.com/hz844Rg.jpg",
                         status: "active",
                         inventory: 2)
 
 merchant_3.items.create(name: "Toy Mouse",
                         description: "Plush 3\" mouse for your owl to play with. Instant fun!",
                         price: 17.99,
-                        image: "/assets/toy_mouse.jpg",
+                        image: "https://i.imgur.com/I5rCHxZ.jpg",
                         status: "active",
                         inventory: 11)
