e1cabee906f7a9c32886b0cfd68933f48c9261ac
Jon Patterson <JonPatt92@gmail.com>
2019-10-04 17:00:53 -0600

    Update merchant and item model tests to represent actual validations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,10 +4,6 @@ describe Merchant, type: :model do
   #validations
   describe "validations" do
     it { should validate_presence_of :name}
-    it { should validate_presence_of :address}
-    it { should validate_presence_of :city}
-    it { should validate_presence_of :state}
-    it { should validate_presence_of :zip}
   end
 
   #relationships
