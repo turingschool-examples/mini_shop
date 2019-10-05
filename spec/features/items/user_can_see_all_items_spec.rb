616e296971d48be5ed67d510b6e48b3e27d5c7b4
Jon Patterson <JonPatt92@gmail.com>
2019-10-04 18:17:09 -0600

    Build items view for index and adjust feature test to look for image source

--- spec/features/items/user_can_see_all_items_spec.rb
+++ spec/features/items/user_can_see_all_items_spec.rb
@@ -19,8 +19,8 @@ describe "Item index page" do
     expect(page).to have_content(item_1.name)
     expect(page).to have_content(item_1.description)
     expect(page).to have_content(item_1.price)
-    expect(page).to have_content(item_1.image)
-    expect(page).to have_content(item_1.status)
+    expect(page).to have_css("img[src*='https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjHy8m84YPlAhUjm-AKHVhoA_IQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.vistaprint.com%2Fmarketing-materials%2Fpens%2Fpremium-ballpoint&psig=AOvVaw0AWcSTG1QUQyoaGTXTktmk&ust=1570318210903655']")
+    expect(page).to have_content("Available")
     expect(page).to have_content(item_1.inventory)
     expect(page).to have_content(merchant_1.name)
 
