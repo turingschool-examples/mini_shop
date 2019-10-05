3d65732e17c55bc228bdf536f1dd5a1eb87735a3
Laura Schulz <lrs8810@gmail.com>
2019-10-05 11:45:12 -0600

    Add edit and update action to merchant controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -15,6 +15,20 @@ class MerchantsController < ApplicationController
     redirect_to '/merchants'
   end
 
+  def edit
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+
+    if merchant.update(merchant_params)
+      redirect_to "/merchants/#{merchant.id}"
+    else
+      redirect_to "/merchants/#{params[:id]}/edit"
+    end
+  end
+
   private
   def merchant_params
     params.permit(:name,:address,:city,:state,:zip)
