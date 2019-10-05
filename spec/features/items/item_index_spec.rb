902b261b4cdc391699cf7b32120ecdf25a39d5b2
Graham Thompson <grwthomps@gmail.com>
2019-10-05 12:14:01 -0600

    Fix test to include correct image url paths

--- spec/features/items/item_index_spec.rb
+++ spec/features/items/item_index_spec.rb
@@ -23,21 +23,21 @@ RSpec.describe "item index page", type: :feature do
     item_1 = merchant_1.items.create(name: "Dog Bed",
                                      description: "Luxury memory foam dog bed for your pupper (dog not included!)",
                                      price: 80,
-                                     image: "/app/assets/images/dog_bed.jpg",
+                                     image: "https://i.imgur.com/D8a4HLN.jpg",
                                      status: "active",
                                      inventory: 7)
 
     item_2 = merchant_2.items.create(name: "Cat Carrier",
                                      description: "Spacious carrier for your feline friend. It will feel like a hotel for your cat! Three zippable flaps.",
                                      price: 27.42,
-                                     image: "/app/assets/images/cat_carrier.jpg",
+                                     image: "https://i.imgur.com/1tX0wB0.jpg",
                                      status: "active",
                                      inventory: 3)
-                                     
+
     item_3 = merchant_3.items.create(name: "Leather Falconry Gloves",
                                      description: "Pristine leather falconry gloves, one size fits all. Superior quality and build. Lifetime warranty.",
                                      price: 149.99,
-                                     image: "/app/assets/images/falconry_gloves.jpg",
+                                     image: "https://i.imgur.com/hz844Rg.jpg",
                                      status: "active",
                                      inventory: 2)
 
@@ -46,7 +46,7 @@ RSpec.describe "item index page", type: :feature do
     expect(page).to have_content(item_1.name)
     expect(page).to have_content(item_1.description)
     expect(page).to have_content(item_1.price)
-    expect(page).to have_css("img[src*='dog_bed']")
+    expect(page).to have_css("img[src*='D8a4HLN']")
     expect(page).to have_content(item_1.status)
     expect(page).to have_content(item_1.inventory)
     expect(page).to have_content(merchant_1.name)
@@ -54,7 +54,7 @@ RSpec.describe "item index page", type: :feature do
     expect(page).to have_content(item_2.name)
     expect(page).to have_content(item_2.description)
     expect(page).to have_content(item_2.price)
-    expect(page).to have_css("img[src*='cat_carrier']")
+    expect(page).to have_css("img[src*='1tX0wB0']")
     expect(page).to have_content(item_2.status)
     expect(page).to have_content(item_2.inventory)
     expect(page).to have_content(merchant_2.name)
@@ -62,7 +62,7 @@ RSpec.describe "item index page", type: :feature do
     expect(page).to have_content(item_3.name)
     expect(page).to have_content(item_3.description)
     expect(page).to have_content(item_3.price)
-    expect(page).to have_css("img[src*='falconry_gloves']")
+    expect(page).to have_css("img[src*='hz844Rg']")
     expect(page).to have_content(item_3.status)
     expect(page).to have_content(item_3.inventory)
     expect(page).to have_content(merchant_3.name)
