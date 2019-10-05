10c2053bfb71438553fdb5926571bc3e0f2dd307
Jon Patterson <JonPatt92@gmail.com>
2019-10-05 17:19:47 -0600

    Improve feature test for story 8 to have multiple merchants and items not belonging to tested merchant

--- spec/features/merchants/user_can_see_merchants_items_spec.rb
+++ spec/features/merchants/user_can_see_merchants_items_spec.rb
@@ -8,6 +8,11 @@ describe "When at merchants/:merchant_id/items" do
                                  city: "Mountain View",
                                 state: "CA",
                                   zip: 90001)
+    merchant_2 = Merchant.create(name: "Google",
+                              address: "456 Greedy Ave",
+                                 city: "Mountain View",
+                                state: "CA",
+                                  zip: 90002)
     item_1 = Item.create(name: "Pen",
                   description: "Ballpoint ink pen",
                         price: 5,
@@ -19,9 +24,17 @@ describe "When at merchants/:merchant_id/items" do
                   description: "Mens wrist watch",
                         price: 110,
                         image: "https://i.imgur.com/owpKw1E.jpg",
-                       status: true,
+                       status: false,
                     inventory: 50,
                     merchant_id: merchant_1.id )
+    item_3 = Item.create(name: "Dress Shoes",
+                  description: "Mens dress shoes",
+                        price: 85,
+                        image: "https://i.imgur.com/vNI9ncq.jpg",
+                       status: true,
+                    inventory: 75,
+                    merchant_id: merchant_2.id )
+
 
     visit "/merchants/#{item_1.merchant_id}/items"
 
@@ -37,6 +50,12 @@ describe "When at merchants/:merchant_id/items" do
     expect(page).to have_css("img[src*='https://i.imgur.com/owpKw1E.jpg']")
     expect(page).to have_content("Unavailable")
     expect(page).to have_content(item_2.inventory)
+
+    expect(page).to_not have_content(merchant_2.name)
+    expect(page).to_not have_content(item_3.name)
+    expect(page).to_not have_content(item_3.price)
+    expect(page).to_not have_css("img[src*='https://i.imgur.com/vNI9ncq.jpg']")
+    expect(page).to_not have_content(item_3.inventory)
 
   end
 end
