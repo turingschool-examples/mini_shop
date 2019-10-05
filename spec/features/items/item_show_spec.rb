53d664ad7fccb53b2e9a6e30c598eba5bf39baaa
Graham Thompson <grwthomps@gmail.com>
2019-10-05 16:34:13 -0600

    Create test for editing item in item show test file

--- spec/features/items/item_show_spec.rb
+++ spec/features/items/item_show_spec.rb
@@ -25,4 +25,41 @@ RSpec.describe "Item show page", type: :feature do
     expect(page).to have_content(item_1.inventory)
     expect(page).to have_content(merchant_2.name)
   end
+
+  it "can edit item attributes" do
+    merchant_2 = Merchant.create(name: "Kitten Market",
+                                 address: "818 Catastic Avenue",
+                                 city: "Orlando",
+                                 state: "FL",
+                                 zip: 32810)
+
+    item_1 = merchant_2.items.create(name: "Cat Carrier",
+                           description: "Spacious carrier for your feline friend. It will feel like a hotel for your cat! Three zippable flaps.",
+                           price: 27.42,
+                           image: "https://i.imgur.com/1tX0wB0.jpg",
+                           status: "active",
+                           inventory: 3)
+
+    visit "/items/#{item_1.id}"
+
+    click_link 'Edit'
+
+    expect(current_path).to eq("/items/#{item_1.id}/edit")
+
+    fill_in 'Name', with: 'Dog Brush'
+    fill_in 'Description', with: 'Premium dog brush to keep your pupper clean and well-groomed. Made with exotic horse hair bristles and rich mahogany.'
+    fill_in 'image_url', with: 'https://i.imgur.com/ZISd7fm.jpg'
+    fill_in 'Price', with: 24.99
+    fill_in 'Inventory', with: 22
+
+    click_button 'Submit'
+
+    expect(current_path).to eq("/items/#{item_1.id}")
+
+    expect(page).to have_content('Dog Brush')
+    expect(page).to have_content('Premium dog brush to keep your pupper clean and well-groomed. Made with exotic horse hair bristles and rich mahogany.')
+    expect(page).to have_css("img[src*='ZISd7fm']")
+    expect(page).to have_content(24.99)
+    expect(page).to have_content(22)
+  end
 end
