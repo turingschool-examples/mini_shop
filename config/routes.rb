7a062e2072350e20992a2b7efbfac373be7ba60a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-05 13:25:08 -0600

    Add items#show route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/merchants/:id/edit', to: 'merchants#edit'
   get '/merchants/:id/items', to: 'merchants#items_index'
   get '/items', to: 'items#index'
+  get '/items/:id', to: 'items#show'
   post '/merchants', to: 'merchants#create'
   patch '/merchants/:id', to: 'merchants#update'
   delete '/merchants/:id', to: 'merchants#delete'
