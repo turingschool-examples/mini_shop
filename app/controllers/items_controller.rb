42d0ab00d9b055e7c540d90bded674df3f243530
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-05 16:10:02 -0600

    Add create action to ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,19 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def create
+    merchant = Merchant.find(params[:id])
+    merchant.items.create!({
+      name: params[:name],
+      description: params[:description],
+      price: params[:price],
+      image: params[:image],
+      inventory: params[:inventory],
+      status: "active",
+      merchant_name: merchant.name
+    })
+
+    redirect_to "/merchants/#{merchant.id}/items"
+  end
 end
