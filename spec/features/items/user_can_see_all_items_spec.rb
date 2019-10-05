21f7d43723e70595545acc22c124d053328026c5
Jon Patterson <JonPatt92@gmail.com>
2019-10-04 18:24:30 -0600

    Adjust status to read properly & replace image with working link, Story 7 complete & passing

--- spec/features/items/user_can_see_all_items_spec.rb
+++ spec/features/items/user_can_see_all_items_spec.rb
@@ -10,16 +10,17 @@ describe "Item index page" do
     item_1 = Item.create(name: "Pen",
                   description: "Ballpoint ink pen",
                         price: 5,
-                        image: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjHy8m84YPlAhUjm-AKHVhoA_IQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.vistaprint.com%2Fmarketing-materials%2Fpens%2Fpremium-ballpoint&psig=AOvVaw0AWcSTG1QUQyoaGTXTktmk&ust=1570318210903655",
+                        image: "https://www.newtimes.co.rw/sites/default/files/styles/mystyle/public/main/articles/2019/07/02/ballpoint-pen.jpg",
                        status: true,
                     inventory: 100,
                     merchant_id: merchant_1.id )
     visit '/items'
+    save_and_open_page
 
     expect(page).to have_content(item_1.name)
     expect(page).to have_content(item_1.description)
     expect(page).to have_content(item_1.price)
-    expect(page).to have_css("img[src*='https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjHy8m84YPlAhUjm-AKHVhoA_IQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.vistaprint.com%2Fmarketing-materials%2Fpens%2Fpremium-ballpoint&psig=AOvVaw0AWcSTG1QUQyoaGTXTktmk&ust=1570318210903655']")
+    expect(page).to have_css("img[src*='https://www.newtimes.co.rw/sites/default/files/styles/mystyle/public/main/articles/2019/07/02/ballpoint-pen.jpg']")
     expect(page).to have_content("Available")
     expect(page).to have_content(item_1.inventory)
     expect(page).to have_content(merchant_1.name)
