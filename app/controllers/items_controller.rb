b015783d39b7df6d55d81aeb2175e99230b40338
Jon Patterson <JonPatt92@gmail.com>
2019-10-05 17:15:05 -0600

    Improve controller action for story 8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   def index
     if params[:merchant_id]
       @merchant = Merchant.find(params[:merchant_id])
-      binding.pry
+      @items = @merchant.items
     else
       @items = Item.all
     end
