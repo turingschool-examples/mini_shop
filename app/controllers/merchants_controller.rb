9965a802ca2fa47c9c9acba0fd937b9c46c5605d
Hillary Stewart <hillarystewart4427@gmail.com>
2019-10-05 08:49:09 -0600

    Add actions to MerchantsController

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,4 +6,41 @@ class MerchantsController < ApplicationController
   def show
     @merchant = Merchant.find(params[:id])
   end
+
+  def new
+  end
+
+  def update
+    merchant = Merchant.find(params[:id])
+
+    merchant.update({name: params[:merchant][:name],
+                                  address: params[:merchant][:address],
+                                  city: params[:merchant][:city],
+                                  state: params[:merchant][:state],
+                                  zip: params[:merchant][:zip]})
+
+    merchant.save
+    redirect_to '/merchants'
+  end
+
+  def create
+    merchant = Merchant.create({name: params[:merchant][:name],
+                              address: params[:merchant][:address],
+                              city: params[:merchant][:city],
+                              state: params[:merchant][:state],
+                              zip: params[:merchant][:zip]})
+
+
+    redirect_to '/merchants'
+  end
+  
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def delete
+    Merchant.delete(params[:id])
+
+    redirect_to '/merchants'
+  end
 end
