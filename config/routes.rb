0adcd133f06a6ea6749d6eb836935c72e27886ee
Jon Patterson <JonPatt92@gmail.com>
2019-10-05 18:24:46 -0600

    Add route for create new item

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/merchants/:merchant_id/items', to: 'items#index'
   get '/items/:id', to: 'items#show'
   get '/merchants/:merchant_id/items/new', to: 'items#new'
+  get '/merchants/:merchant_id/items', to: 'items#create'
 end
