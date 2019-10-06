280046f81c6e077c448a3df7c1c268f7c8209566
Rachel Lew <rlew07@gmail.com>
2019-10-06 13:19:32 -0600

    Add model test for Item

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Item, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :image }
+    it { should validate_inclusion_of (:active?).in_array([true, false])}
+    it { should validate_presence_of :inventory }
+  end
+end
