4f632451f0f14679c4a03c159d559e972bda2547
Graham Thompson <grwthomps@gmail.com>
2019-10-06 15:36:47 -0600

    Refactor new merchant and edit merchant tests

--- spec/features/merchants/create_new_merchants_spec.rb
+++ spec/features/merchants/create_new_merchants_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "creating a merchant", type: :feature do
-  it "can click a link to create a new merchant" do
+  it "create a new merchant" do
     visit "/merchants"
 
     expect(page).to have_link("Create New Merchant")
@@ -9,22 +9,12 @@ RSpec.describe "creating a merchant", type: :feature do
     click_link "Create New Merchant"
 
     expect(current_path).to eq('/merchants/new')
-  end
 
-  it "can create a new merchant" do
-    visit "/merchants/new"
-
-    expect(page).to have_field("merchant[name]")
-    expect(page).to have_field("merchant[address]")
-    expect(page).to have_field("merchant[city]")
-    expect(page).to have_field("merchant[state]")
-    expect(page).to have_field("merchant[zip]")
-
-    fill_in 'merchant[name]', with: "Doug's Donuts"
-    fill_in 'merchant[address]', with: '123 Fried Dough Drive'
-    fill_in 'merchant[city]', with: 'Portland'
-    fill_in 'merchant[state]', with: 'OR'
-    fill_in 'merchant[zip]', with: 12345
+    fill_in 'Name', with: "Doug's Donuts"
+    fill_in 'Address', with: '123 Fried Dough Drive'
+    fill_in 'City', with: 'Portland'
+    fill_in 'State', with: 'OR'
+    fill_in 'Zip', with: 12345
 
     click_button 'Submit'
 
