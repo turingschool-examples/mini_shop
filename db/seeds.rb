c00c31e1f25b276ca8468b0b72607885ef0d8e22
Laura Schulz <lrs8810@gmail.com>
2019-10-06 03:51:32 -0600

    Add items table migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-merchants = Merchant.create([{ name: 'Mobilitas', address: '123 Main Street', city: 'Denver', state: 'CO', zip: 80218 },
-                             { name: 'Sedona', address: '346 Hope Ave', city: 'San Antonio', state: 'TX', zip: 78240 },
-                             { name: 'Star Fitness', address: '75 Springride Meadows', city: 'Los Angeles', state: 'CA', zip: 90040}])
+merchants = Merchant.create([{ name: 'Valeo', address: '123 Main Street', city: 'Denver', state: 'CO', zip: 80218 },
+                             { name: 'Spri', address: '346 Hope Ave', city: 'San Antonio', state: 'TX', zip: 78240 },
+                             { name: 'Mobilitas', address: '75 Springride Meadows', city: 'Los Angeles', state: 'CA', zip: 90040}])
+
+item_1 = merchants.first.items.create(name: 'Medicine Ball',
+                                    description: 'This mb is awesome!',
+                                    price: 45,
+                                    image_url: 'https://i.imgur.com/pLRfskZ.jpg',
+                                    active: true,
+                                    inventory: 34)
+
+item_2 = merchants[1].items.create(name: 'Kettlebell',
+                                  description: 'This kb is awesome!',
+                                  price: 33.40,
+                                  image_url: 'https://i.imgur.com/BVEkfc7.jpg',
+                                  active: true,
+                                  inventory: 10)
