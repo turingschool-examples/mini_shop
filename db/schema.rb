f248a6756d940f122f8eb9b29b02ec1a5f142be9
Rachel Lew <rlew07@gmail.com>
2019-10-06 13:11:48 -0600

    Migrate Items table into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191002143206) do
+ActiveRecord::Schema.define(version: 20191006185924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "price"
+    t.string "image"
+    t.boolean "active?", default: true
+    t.integer "inventory"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
