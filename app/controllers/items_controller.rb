fa78df180978d36188260534c0efd483e457ff34
David Lopez <ldavid.x.lopezl@gmail.com>
2019-10-05 19:15:58 -0600

    Create for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,25 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:id])
+    merchant.items.create(item_params)
+    redirect_to "/merchants/#{merchant.id}/items"
+  end
+
+  private
+      def item_params
+        params.permit(
+          :name,
+          :description,
+          :price,
+          :image,
+          :inventory
+        )
+      end
 end
