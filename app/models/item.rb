4810290c718af06c33867c405496c91df92ba745
Rachel Lew <rlew07@gmail.com>
2019-10-06 13:59:18 -0600

    Add validates presence of column names

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  validates_presence_of :name,
+                        :description,
+                        :price,
+                        :image,
+                        :active?,
+                        :inventory
 end
