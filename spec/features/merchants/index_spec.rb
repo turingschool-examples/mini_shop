fa431b80200e21596fa8180ccb1a807551075a54
Laura Schulz <lrs8810@gmail.com>
2019-10-06 13:56:41 -0600

    Refactor spec spacing for merchants index

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe "merchants index page", type: :feature do
     merchant_1 = Merchant.create(name: "Sedona", address: '123 Main Street', city: 'Denver', state: 'CO', zip: 80218)
     merchant_2 = Merchant.create(name: "Fun Fitness", address: '75 Springride Meadows', city: 'Los Angeles', state: 'CA', zip: 90040)
 
-  visit "/merchants"
+    visit "/merchants"
 
-  expect(page).to have_link("Sedona")
-  expect(page).to have_link("Fun Fitness")
+    expect(page).to have_link("Sedona")
+    expect(page).to have_link("Fun Fitness")
   end
 end
