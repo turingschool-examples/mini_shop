175621f6c9a00a116796d23bdea09ca49a7e5720
Daniel <danmoran.pro@gmail.com>
2020-01-02 19:37:08 -0700

    Testing, Model, Controller complete for Merchant index.

---
+++ spec/features/merchant/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "As a vistior" do
+    describe "when I visit '/merchants'" do
+      it "Can see the name of each merchant" do
+        apple_nyc = Merchant.create!(name: "Apple NYC", address: "123 Bear st.", city: "Denver", state: "CO", zip: "80220")
+
+        visit '/merchants'
+
+        expect(page).to have_content(apple_nyc.name)
+    end
+  end
+end
