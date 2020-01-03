01d40c3365f8b68e358f59cd4cd691d9a8453ba7
Daniel <danmoran.pro@gmail.com>
2020-01-02 19:40:37 -0700

    Merchant index view currently showing

--- spec/features/merchant/index_spec.rb
+++ spec/features/merchant/index_spec.rb
@@ -6,7 +6,6 @@ describe "As a vistior" do
         apple_nyc = Merchant.create!(name: "Apple NYC", address: "123 Bear st.", city: "Denver", state: "CO", zip: "80220")
 
         visit '/merchants'
-
         expect(page).to have_content(apple_nyc.name)
     end
   end
